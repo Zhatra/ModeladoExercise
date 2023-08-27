b8c988098757920d6e4545793b7e3e430debfa64
Christian <chrtaw@outlook.com>
2023-08-26 23:50:16 -0600

    s

--- ejercicios_en_clase/convertordenumerosromanos.py
+++ ejercicios_en_clase/convertordenumerosromanos.py
@@ -1,8 +1,8 @@
-test1 ="XV"
-valores = {"X":10,"V":5}
+test1 ="VI"
+valores = {"X":10,"V":5,"I":1,"L":100}
 test1 = list(test1)
 res=0
-print(test1)
 for i in test1:
-    valores[v]
-    print (i)
+    res += valores[i]
+print(res)
+#s
